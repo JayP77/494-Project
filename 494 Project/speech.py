d72c749c432a80f2abe11bd74e627c1cbd391ac2
JayP77 <60245037+JayP77@users.noreply.github.com>
2022-10-25 20:07:04 -0500

    all commands work, gui fixed

--- 494 Project/speech.py
+++ 494 Project/speech.py
@@ -1,5 +1,14 @@
+import audioop
+import posix
+from sunau import AUDIO_UNKNOWN_SIZE
 import speech_recognition as sr
 import dearpygui.dearpygui as dpg
+
+posX = 0
+posY = 0
+
+def substring_after(s, delim):
+    return s.partition(delim)[2]
 
 def parse(str):
   create = "create "
@@ -30,40 +39,115 @@ def parse(str):
 dpg.create_context()
 dpg.create_viewport()
 dpg.setup_dearpygui()
-
+listOfReminders=[]
+reminders=dict({})
+reminderToAdd=""
+reminderToRemove=""
+reminder=""
 # Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
+str = ""
+def edit_helper(reminderToEdit,reminders):
+  r = sr.Recognizer()
+  with(dpg.window(label='EDIT', pos=(0,150), width = 1440)):
+    dpg.add_text('What should I change the time to?')
+  with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-    print (r.recognize_google(audio))
-    #print("You said: " + r.recognize_google(audio))
-# print("a")
-# Speech recognition using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
+    audio=r.listen(source)
     str = r.recognize_google(audio)
-    reminder = parse(str)
-    print("You said: " + str)
+    #print("You said: " + str)
+    return str
+newStr = ""
+def speak_callback():
+  with sr.Microphone() as source:
+      r = sr.Recognizer()
+      print("Say something!")
+      r.adjust_for_ambient_noise(source)
+      audio = r.listen(source)
+      print (r.recognize_google(audio))
+      #print("You said: " + r.recognize_google(audio))
+  # print("a")
+  # Speech recognition using Google Speech Recognition
+  try:
+      # for testing purposes, we're just using the default API key
+      # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+      # instead of `r.recognize_google(audio)`
+      str = r.recognize_google(audio)
+      reminder = parse(str)
+      
+      if("add reminder" in str or 'new reminder' in str):
+        myString = parse(str)
+        if('at' not in str):
+          reminderToAdd=substring_after(str,'reminder')
+          reminderToAdd=reminderToAdd.strip()
+          #listOfReminders.append(reminderToAdd)
+          reminders[reminderToAdd]='NO TIME GIVEN'
+        else:
+          reminderToAdd=substring_after(str,'reminder')
+          reminderToAdd=reminderToAdd.split('at',1)[0]
+          timeToAdd = substring_after(str,'at')
+          reminderToAdd=reminderToAdd.strip()
+          #listOfReminders.append(reminderToAdd)
+          reminders[reminderToAdd]= timeToAdd
+      if("remove reminder" in str):
+        myString = parse(str)
+        reminderToRemove=substring_after(str,'reminder')
+        reminderToRemove=reminderToRemove.strip()
+        #listOfReminders.remove(reminderToRemove)
+        if(reminderToRemove in reminders):
+          reminders.pop(reminderToRemove)
+      if("edit reminder" in str):
+        myString = parse(str)
+        reminderToEdit=substring_after(str,'reminder')
+        reminderToEdit=reminderToEdit.strip()
+        if(reminderToEdit in reminders):
+          newTime=edit_helper(reminderToEdit,reminders)
+          print("NEW TIME IS: " + newTime)
+        reminders[reminderToEdit]=newTime
+        
 
-    
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    # dpg.add_text("Add - Add reminder x at y")
+    # dpg.add_text("Remove - Remove reminder x")
+    # dpg.add_text("Edit - Edit reminder x, new x")
+    #dpg.add_text(r.recognize_google(audio))
+      print("You said: " + str)
+      with dpg.window(label="Feedback", pos=(0, 720), width = 1440):
+        # global newStr
+        dpg.add_text("You said: " + str)
+      global newStr
+      newStr = "ABC"
+      print("TESTING: ")
+      print(*listOfReminders,sep=', ')
+      #with dpg.window(label="reminder"):
+      #  for i in listOfReminders:
+      #    dpg.add_text(i)
+
+      with dpg.window(label='reminder', pos=(0,150), width = 1440, height = 570):
+        for i in reminders:
+          mStr=i+" " + reminders[i]
+          dpg.add_text(mStr)
+          
+      print(reminders)
+
+      
+  except sr.UnknownValueError:
+      print("Google Speech Recognition could not understand audio")
+  except sr.RequestError as e:
+      print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 def save_callback():
     print("Save Clicked")
+   #a = str
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello world")
-    dpg.add_text(r.recognize_google(audio))
-    dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
+
+
+with dpg.window(label="speak", pos=(620, 0), height=150):
+    dpg.add_text("Press Speak before giving command")
+    dpg.add_text("Add - Add reminder x at y")
+    dpg.add_text("Remove - Remove reminder x")
+    dpg.add_text("Edit - Edit reminder x, new x")
+    #dpg.add_text(r.recognize_google(audio))
+
+    dpg.add_button(label="SPEAK", callback=speak_callback, width= 250)
 
 # make sure we got a command
 if reminder != "":
